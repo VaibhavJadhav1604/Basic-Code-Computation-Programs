7c44d99d3e0756bcc0c9f95338dfda7538032797
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-08 08:00:14 +0530

    Factors Of Given Number
    Program To Find Factors Of Given Number

---
+++ FN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FactorsOfNumber
+{
+    internal class FN
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int factor;
+            Console.Write("Factors Are:");
+            for (factor = 1; factor <= num; factor++)
+            {
+                if (num % factor == 0)
+                {
+                    Console.Write(factor +",");
+                }
+            }
+        }
+    }
+}
