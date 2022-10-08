44873032d19fadcc0ae2cc5b8acd7647b65599e0
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-08 08:21:39 +0530

    Program To Swap Two Number Values
    Program To Swap Two Number Values

---
+++ STN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    internal class STN
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2, c=0;
+            Console.WriteLine("Before Swaping Values Are A:"+ a +" B:"+ b);
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("After Swaping Values Are A:" + a + " B:" + b);
+        }
+    }
+}
